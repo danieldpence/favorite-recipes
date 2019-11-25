ac045b5cd8e86338288c7ef06a502e1e8817caf6
Daniel Pence <danieldpence6@gmail.com>
2019-11-25 16:24:30 -0600

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+demo_user = User.create!(name: Faker::Name.name, email: "demo@example.com", password: "password", password_confirmation: "password")
+
+10.times do
+  recipe = Recipe.create!({
+    name: Faker::Food.dish,
+    description: Faker::Food.description,
+    instructions: "",
+    time_to_prepare: "#{rand(15..120)} minutes",
+    feeds: "4-6",
+    user: demo_user})
+
+  5.times do
+    Ingredient.create!({
+      recipe: recipe,
+      quantity: Faker::Food.measurement,
+      name: Faker::Food.ingredient
+    })
+  end
+end
