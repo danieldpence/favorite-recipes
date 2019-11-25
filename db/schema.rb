55a3adc42af2e04b7527700880c19b4ea822f354
Daniel Pence <danieldpence6@gmail.com>
2019-11-25 15:59:55 -0600

    Add devise Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_22_205756) do
+ActiveRecord::Schema.define(version: 2019_11_25_212533) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "quantity"
@@ -29,7 +29,23 @@ ActiveRecord::Schema.define(version: 2019_11_22_205756) do
     t.string "feeds"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "ingredients", "recipes"
+  add_foreign_key "recipes", "users"
 end
