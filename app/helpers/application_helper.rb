b0e961b43245c265e9f99ef235c45eced15ac2bd
Daniel Pence <danieldpence6@gmail.com>
2019-11-29 16:57:43 -0600

    Add user avatar uploads

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_avatar(user, size=80)
+    if user.avatar.attached?
+      user.avatar.variant(resize: "#{size}x#{size}!")
+    else
+      'placeholder-person.png'
+    end
+  end
 end
