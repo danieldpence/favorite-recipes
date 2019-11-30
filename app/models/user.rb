ddb99bd1eceab7e4b482aaf847dcb5ab47909d87
Daniel Pence <danieldpence6@gmail.com>
2019-11-29 20:21:06 -0600

    Style the edit user registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_many :recipes
+  has_many :recipes, dependent: :destroy
   has_one_attached :avatar
 end
